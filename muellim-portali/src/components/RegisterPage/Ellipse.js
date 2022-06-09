4cdf920f8fb290403611e25168e45f6048b3ad20
Asif-vg <asifvg@code.edu.az>
2022-06-09 16:13:14 +0400

    some add

--- muellim-portali/src/components/RegisterPage/Ellipse.js
+++ muellim-portali/src/components/RegisterPage/Ellipse.js
@@ -1,10 +1,13 @@
-import React, { useState } from 'react'
-import ImgUpload from "./ImgUpload";
+import React, {useRef,useState } from 'react'
+import {FaRegEdit} from "react-icons/fa";
 
 function Ellipse() {
+  const profileRef = useRef();
+  const [view,setView]=useState(true);
   const [upload,setUpload] = useState({ file: ''}) 
-  const photoUpload = e =>{
-    e.preventDefault();
+  const {imagePreviewUrl} = upload;
+
+  function handleChangeImg(e){
     const reader = new FileReader();
     const file = e.target.files[0];
     reader.onloadend = () => {
@@ -14,13 +17,28 @@ function Ellipse() {
       });
     }
     reader.readAsDataURL(file);
+
+    if(profileRef.current.value.length > 0){
+      if(view){
+        setView(!view)
+      }
+    }
+    
   }
-  const {imagePreviewUrl} = upload;
   return (
     <>
-    {(
-      <ImgUpload onChange={photoUpload} src={imagePreviewUrl}/>
-    )}
+    <label htmlFor="photo-upload" className="custom-file-upload">
+      <div className="img-wrap img-upload" >
+        <img className='img-fluid'  src={imagePreviewUrl} alt=""/>
+      </div>
+      <input id="photo-upload" type="file"  ref={profileRef} onChange={(e)=>handleChangeImg(e)}/> 
+      <div className="ellipse-icon">
+        <FaRegEdit />
+      </div>
+      {
+        view ? <span className='enter-img'>Profil şəkli əlavə et </span> : ""
+      } 
+    </label>
   </>
   )
 }
